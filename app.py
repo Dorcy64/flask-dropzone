cc5a154acd2f7d6689b16875f159420b6ddd5bc1
Dorcy Shema <42618844+Dorcy64@users.noreply.github.com>
2021-05-03 15:59:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-    :author: Grey Li <withlihui@gmail.com>
-    :copyright: (c) 2017 by Grey Li.
-    :license: MIT, see LICENSE for more details.
-"""
 import os
 
 from flask import Flask, render_template, request
@@ -17,7 +11,7 @@ app.config.update(
     UPLOADED_PATH=os.path.join(basedir, 'uploads'),
     # Flask-Dropzone config:
     DROPZONE_ALLOWED_FILE_TYPE='image',
-    DROPZONE_MAX_FILE_SIZE=3,
+    DROPZONE_MAX_FILE_SIZE=10,
     DROPZONE_MAX_FILES=30,
     DROPZONE_IN_FORM=True,
     DROPZONE_UPLOAD_ON_CLICK=True,
